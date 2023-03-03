b1219b524906f739e95ee005539b9ea98f7a6c36
akshaymrathod <126705089+akshaymrathod@users.noreply.github.com>
2023-03-03 21:52:41 +0530

    Add files via upload
    initial draft of portfolio

---
+++ checkblack.js
@@ -0,0 +1,15 @@
+
+            function checkforblank(){
+                if(document.getElementById('Name').value == "") {
+                alert("please enter  your name");
+                return false;
+            }
+            if(document.getElementById('Email').value == "") {
+                alert("please enter  your email id");
+                return false;
+            }
+            if(document.getElementById('Contact No').value == "") {
+                alert("please enter  your Mobile number");
+                return false;
+            }
+            }
